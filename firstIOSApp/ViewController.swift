63ce35da288f415a6c70efc7ff2495d9f142f9d0
cdmccune <curtmccune@gmail.com>
2022-05-17 16:29:48 -0700

    completed first application

--- firstIOSApp/ViewController.swift
+++ firstIOSApp/ViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var ageLabel: UILabel!
+    @IBOutlet var profileImage: UIImageView!
+    @IBOutlet var bioLabel: UILabel!
+    
+    func setUpAboutMe() {
+        nameLabel.text = "Curt McCune"
+        titleLabel.text = "Athlete, Friend, Developer"
+        ageLabel.text = "Age: 24"
+        profileImage.image = UIImage(named: "curtProfile")
+        bioLabel.text = "Bio: As Curt McCune grew up, few would know where he would end up. It seemed like a straight path to become an engineer, but just a couple of decisions later he ended up in Eastern Europe as a missionary. Many different experiences were had and you could even say that Curt himself changed. Now back in his hometown, everyone is waiting to see what he will do next."
+    }
+    
     override func viewDidLoad() {
+       
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setUpAboutMe()
     }
 
 
